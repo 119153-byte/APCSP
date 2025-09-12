f3f4616a81d7d15c37b204d0fb87d7310405aab2
Yukahn-Pestis <140148482+Yukahn-Pestis@users.noreply.github.com>
2025-09-12 10:21:43 -0700

    Create create_task.py

---
+++ create_task.py
@@ -0,0 +1,18 @@
+#library of Alexandria
+
+import random
+
+"""The purpose of this program is to recommend users books from the programmer's current library
+        - we begin with two lists, one providing the recommended titles, another providing their authors
+        - Then, the program will ask the user to decide whether they want to recieve  a book recommendation or to provide one.
+"""
+recommended_books = ["S. ", "Odyssey", "House of leaves", "Fall of the house of Cabal -- Johnonthan L. Howard", "The Devil and Ms. Prym -- Paulo Coehlo"]
+authors = ["Doug Dorst & JJ Abrams", "Homer", "Mark Z. Danielewski", "Johnathan L. Howard", "Paulo Coehlo"]
+user_recommended = []
+
+user_option = str()
+books_recommended = 0
+
+
+print("Welcome to the Library \n ")
+
