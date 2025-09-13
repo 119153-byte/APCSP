7ec0eb5ca63151b9582ed68e8ce76552fb6defbc
Yukahn-Pestis <140148482+Yukahn-Pestis@users.noreply.github.com>
2025-09-13 05:08:26 +0000

    defined OPTION1 Function and cleaned up previous lines.

--- create_task.py
+++ create_task.py
@@ -7,10 +7,9 @@ import time
         - we begin with two lists, one providing the recommended titles, another providing their authors
         - Then, the program will ask the user to decide whether they want to recieve  a book recommendation or to provide one.
 """
-recommended_books = ["S. ", "Odyssey", "House of leaves", "Fall of the house of Cabal -- Johnonthan L. Howard", "The Devil and Ms. Prym -- Paulo Coehlo"]
-authors = ["Doug Dorst & JJ Abrams", "Homer", "Mark Z. Danielewski", "Johnathan L. Howard", "Paulo Coehlo"]
+recommended_books = ["S. ", "Odyssey", "House of leaves", "Fall of the house of Cabal", "The Devil and Ms. Prym", "Moby Dick", "Don Quixote", "Othello", "Ulysses", "The Giver"]
+authors = ["Doug Dorst & JJ Abrams", "Homer", "Mark Z. Danielewski", "Johnathan L. Howard", "Paulo Coehlo", "Herman Melville", "Miguel De Cervantes", "Shakespeare", "James Joyce", "Lois Lowry"]
 user_recommended = []
-
 user_option = None
 books_recommended = 0
 
@@ -22,7 +21,6 @@ def m_menu(u_opt):
         print("In the library, you can either: \n 1. Recieve a Book Recommendation \n 2. Give a Book Recommendation \n 3. Exit")
         time.sleep(1)
         print("Input your choice of (1) (2) (3) \n")
-        time.sleep(1)
         while True:
                 u_opt = input("Your choice: ")
                 try:
@@ -33,24 +31,33 @@ def m_menu(u_opt):
                 else:
                         print("Please enter a valid number (1, 2, 3): ")
         if int(u_opt) == 1:
-                OPTION1()
+                OPTION1(user_option=None)
         elif int(u_opt) == 2:
                 OPTION2()
         elif int(u_opt) == 3:
-                OPTION3()
-    
-                
-def OPTION1():
-        print('1')
+                print("Closing program....Goodbye!")
+                time.sleep(3)
 
-
+def OPTION1(user_option):  #User recieves book recommendation
+        i = random.randint(0, 9)
+        print("The book of choice is...")
+        time.sleep(1)
+        print(f"{recommended_books[i]} by {authors[i]} \n Will that be all? \n Y/N")
+        while True:
+                user_option = input("Your Answer: ")
+                try:
+                        if user_option.upper() in ["Y", "N"]:
+                                break
+                except ValueError:
+                        print("Please provide a valid answer: ")
+                else:
+                        print("Please provide a valid answer: ")
+        if user_option.upper() == "Y":
+                print("Closing Program....Goodbye!")
+        else:
+                print("Redirecting to Main Menu...")
+                m_menu(user_option)
 def OPTION2():
         print('2')
-
-
-def OPTION3():
-        print("3")
-
-
 m_menu(user_option)
 print("program ended")
