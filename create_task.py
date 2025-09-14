d4cb6145556de8d98fe3a8cfe14efaf4b694cd95
Yukahn-Pestis <140148482+Yukahn-Pestis@users.noreply.github.com>
2025-09-14 05:45:35 +0000

    had a crazy headache 10 minutes in. 90% sure I wont even remember what changes have been made. goodluck future me i guess...

--- create_task.py
+++ create_task.py
@@ -10,39 +10,34 @@ import time
 recommended_books = ["S. ", "Odyssey", "House of leaves", "Fall of the house of Cabal", "The Devil and Ms. Prym", "Moby Dick", "Don Quixote", "Othello", "Ulysses", "The Giver"]
 authors = ["Doug Dorst & JJ Abrams", "Homer", "Mark Z. Danielewski", "Johnathan L. Howard", "Paulo Coehlo", "Herman Melville", "Miguel De Cervantes", "Shakespeare", "James Joyce", "Lois Lowry"]
 user_recommended = []
-user_option = None
 books_recommended = 0
-
 
 print("#####WELCOME TO THE LIBRARY######") #only asked once because its the initial start of the program
 
-def m_menu(u_opt):
+def m_menu():
         time.sleep(1)
-        print("In the library, you can either: \n 1. Recieve a Book Recommendation \n 2. Give a Book Recommendation \n 3. Exit")
+        print("In the library, you can either: \n 1. Recieve a Book Recommendation \n 2. Give a Book Recommendation \n 3. Exit") #menu screen
         time.sleep(1)
         print("Input your choice of (1) (2) (3) \n")
-        while True:
-                u_opt = input("Your choice: ")
+        while True: #while loop to ensure the user's response doesnt break program with undesired response
+                user_option = input("Your choice: ")
                 try:
-                        if int(u_opt) in [1, 2, 3]:
-                                break
+                        if int(user_option) in [1, 2, 3]: #when a user's input fulfills the parameter it continues the program
+                                break 
                 except ValueError:
                         print("Please enter a valid number (1, 2, 3): ")
                 else:
                         print("Please enter a valid number (1, 2, 3): ")
-        if int(u_opt) == 1:
-                OPTION1(user_option=None)
-        elif int(u_opt) == 2:
+        if int(user_option) == 1:
+                OPTION1()
+        elif int(user_option) == 2:
                 OPTION2()
-        elif int(u_opt) == 3:
+        elif int(user_option) == 3:
                 print("Closing program....Goodbye!")
-                time.sleep(3)
+                time.sleep(1)
 
-def OPTION1(user_option):  #User recieves book recommendation
-        i = random.randint(0, 9)
-        print("The book of choice is...")
-        time.sleep(1)
-        print(f"{recommended_books[i]} by {authors[i]} \n Will that be all? \n Y/N")
+def redirect(): #asks user whether to continue or close program at the end of each menu
+        print(" Will that be all? \n Y/N")
         while True:
                 user_option = input("Your Answer: ")
                 try:
@@ -56,8 +51,29 @@ def OPTION1(user_option):  #User recieves book recommendation
                 print("Closing Program....Goodbye!")
         else:
                 print("Redirecting to Main Menu...")
-                m_menu(user_option)
-def OPTION2():
-        print('2')
-m_menu(user_option)
-print("program ended")
+                print(' \n' * 12) #clears screen
+                m_menu()
+
+def OPTION1():  #User receives book recommendation
+        i = random.randint(0, 9)
+        print("The book of choice is...")
+        time.sleep(1)
+        print(f"{recommended_books[i]} by {authors[i]} \n")
+        redirect()
+       
+
+def OPTION2(): #User provides book recommendation
+        global user_recommended
+        global books_recommended
+        print("Please provide a book recomendation below!")
+        rec  = input("Your Answer: ")
+        user_recommended.append(rec)
+        print("\n Thank you for the recommendation! \n It has been added to the list! \n ")
+        books_recommended += 1
+        redirect() #maybe create a loop to redo this option without going to m_menu?
+        
+m_menu()
+
+print("You've recommended a total of [", str(books_recommended), "] books. \n they include: ", user_recommended)
+
+print("Program terminated")
