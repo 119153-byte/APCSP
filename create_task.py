85779a73010bc149aebf06cb2d49b6fc86b3e53f
Yukahn-Pestis <140148482+Yukahn-Pestis@users.noreply.github.com>
2025-09-25 10:42:01 -0700

    Update create_task.py
    commiting through the application itself because my codespace be buggin

--- create_task.py
+++ create_task.py
@@ -3,10 +3,6 @@
 import random
 import time
 
-"""The purpose of this program is to recommend users books from the programmer's current library
-        - we begin with two lists, one providing the recommended titles, another providing their authors
-        - Then, the program will ask the user to decide whether they want to recieve  a book recommendation or to provide one.
-"""
 recommended_books = ["S. ", "Odyssey", "House of leaves", "Fall of the house of Cabal", "The Devil and Ms. Prym", "Moby Dick", "Don Quixote", "Othello", "Ulysses", "The Giver"]
 authors = ["Doug Dorst & JJ Abrams", "Homer", "Mark Z. Danielewski", "Johnathan L. Howard", "Paulo Coehlo", "Herman Melville", "Miguel De Cervantes", "Shakespeare", "James Joyce", "Lois Lowry"]
 user_recommended = []
@@ -36,31 +32,54 @@ def m_menu():
                 print("Closing program....Goodbye!")
                 time.sleep(1)
 
-def redirect(): #asks user whether to continue or close program at the end of each menu
-        print(" Will that be all? \n Y/N")
-        while True:
-                user_option = input("Your Answer: ")
-                try:
-                        if user_option.upper() in ["Y", "N"]:
-                                break
-                except ValueError:
-                        print("Please provide a valid answer: ")
+
+def redirect(option): #asks user whether to continue or close program at the end of each menu
+        if option == 0:
+                print(" Will that be all? \n Y/N")
+                while True:
+                        user_option = input("Your Answer: ")
+                        try:
+                                if user_option.upper() in ["Y", "N"]:
+                                        break
+                        except ValueError:
+                                print("Please provide a valid answer: ")
+                        else:
+                                print("Please provide a valid answer: ")
+                if user_option.upper() == "Y":
+                        print("Closing Program....Goodbye!")
                 else:
-                        print("Please provide a valid answer: ")
-        if user_option.upper() == "Y":
-                print("Closing Program....Goodbye!")
-        else:
-                print("Redirecting to Main Menu...")
-                print(' \n' * 12) #clears screen
-                m_menu()
+                        print("Redirecting to Main Menu...")
+                        print(' \n' * 12) #clears screen
+                        m_menu()
+        else: 
+                print("Would You likke to: \n a. Provide a New Recommendation \n b. Exit to Main Menu \n c. Exit Program \n")
+                while True:                        
+                        user_option = input("Your Answer: ")
+                        try:
+                                if user_option.lower() in ['a', 'b', 'c']:
+                                        break
+                        except ValueError:
+                                print("Please provide a valid answer: ")
+                        else:
+                                print("Please provide a valid answer: ")
+                if user_option.lower() == 'a':
+                        OPTION2()
+                elif user_option.lower() == 'b':
+                        print("Redirecting to Main Menu...")
+                        print(' \n' * 12) #clears screen
+                        m_menu()
+                else:
+                        return 0 #exits program
+
 
 def OPTION1():  #User receives book recommendation
         i = random.randint(0, 9)
         print("The book of choice is...")
         time.sleep(1)
         print(f"{recommended_books[i]} by {authors[i]} \n")
-        redirect()
-       
+        ri = 0
+        redirect(ri) 
+
 
 def OPTION2(): #User provides book recommendation
         global user_recommended
@@ -70,10 +89,12 @@ def OPTION2(): #User provides book recommendation
         user_recommended.append(rec)
         print("\n Thank you for the recommendation! \n It has been added to the list! \n ")
         books_recommended += 1
-        redirect() #maybe create a loop to redo this option without going to m_menu?
-        
+        ri = 1 #redirect index to change the option screen of rediect function
+        redirect(ri)
+
+
 m_menu()
 
-print("You've recommended a total of [", str(books_recommended), "] books. \n they include: ", user_recommended)
-
-print("Program terminated")
+if books_recommended > 0:
+        print("You've recommended a total of [", str(books_recommended), "] books. \n they include: ", user_recommended)
+print("Program Terminated")
